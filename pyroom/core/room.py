a18686db503aa749503580465c0b94cdb57233de
landpack <ada.frank.ak@gmail.com>
2016-12-17 17:13:37 +0800

    [add] book & cancel

--- pyroom/core/room.py
+++ pyroom/core/room.py
@@ -105,14 +105,70 @@ class BaseRoomManager(object):
 
 
 class RoomManager(BaseRoomManager):
+    room_to_uid_set = {}
+    uid_to_room = {}
+    room_lack_level = {}
+    lack_level_set = {}
+    room_name_index = 0
+
+    @classmethod
+    def init_lack_level_set(cls, size=9):
+        for i in range(1, size + 1):
+            cls.lack_level_set[i] = set()
+
+    @classmethod
+    def gen_room_name(cls, prefix='room_'):
+        room_name = prefix + str(cls.room_name_index)
+        cls.room_name_index += 1
+        return room_name
+
+    @classmethod
+    def is_not_full(cls):
+        pass
+
+    @classmethod
+    def new_room(cls, uid, size=9):
+        room_name = cls.gen_room_name()
+        cls.room_to_uid_set[room_name] = set()
+        cls.room_to_uid_set[room_name].add(uid)
+        cls.room_lack_level[room_name] = size - 1
+        cls.lack_level_set[size - 1].add(room_name)
+        return room_name
+
+    @classmethod
+    def old_room(cls, uid, size=9):
+        for r in range(1, size + 1):
+            set_length = len(cls.lack_level_set[r])
+            if set_length > 0:
+                room_name = cls.lack_level_set[r].pop()
+                lack_level = cls.room_lack_level[room_name]
+                if lack_level == 1:
+                    del cls.room_lack_level[room_name]
+                else:
+                    cls.room_lack_level[room_name] = lack_level - 1
+                    cls.lack_level_set[r - 1].add(room_name)
+                cls.room_to_uid_set[room_name].add(uid)
+                return room_name
+        raise ValueError("No old room available!")
+
     @classmethod
     def book(cls, uid):
+        try:
+            room_name = cls.old_room(uid)
+        except ValueError:
+            room_name = cls.new_room(uid)
         cls.set_ttl(uid)
-        print 'book a room for', uid
+        cls.uid_to_room[uid] = room_name
+        return room_name
 
     @classmethod
     def cancel(cls, uid):
-        pass
+        room_name = cls.uid_to_room[uid]
+        lack_level = cls.room_lack_level[room_name]
+        cls.room_to_uid_set[room_name].remove(uid)
+        cls.lack_level_set[lack_level].remove(room_name)
+        cls.lack_level_set[lack_level + 1].add(room_name)
+        cls.room_lack_level[room_name] = lack_level + 1
 
     @classmethod
     def check_in(cls, uid):
